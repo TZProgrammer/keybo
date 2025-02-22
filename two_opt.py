ce7278c55647c12046b69dcce6b3d18feeadf454
TZProgrammer <thomaszeger@gmail.com>
2025-02-22 00:46:06 -0500

    implement two_opt script

---
+++ two_opt.py
@@ -0,0 +1,23 @@
+from optimizer import Optimizer
+from classifier import Keyboard
+from scorer import BigramXGBoostScorer
+from wpm_conditioned_model import TypingModel
+
+qwerty = "qwertyuiopasdfghjkl'zxcvbnm,.-"
+tusk_1 = "tskcwjqioumvypznreaxlfgdbh-'.,"
+
+def generate():
+    layout = tusk_1
+    keyboard = Keyboard(layout)
+    scorer = BigramXGBoostScorer()
+    optimizer = Optimizer(keyboard, scorer)
+    optimizer.local_improvement_2opt(keyboard, scorer)
+    print("Fitness", optimizer.best_fitness)
+    print(optimizer.best_keyboard)
+
+
+def main():
+    generate()
+
+if __name__ == "__main__":
+    main()
