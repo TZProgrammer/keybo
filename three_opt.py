0920d14c0ad87c054d1b5d894ed7277d84dcb7a6
TZProgrammer <thomaszeger@gmail.com>
2025-02-22 01:37:57 -0500

    add three_opt script

---
+++ three_opt.py
@@ -0,0 +1,24 @@
+from optimizer import Optimizer
+from classifier import Keyboard
+from scorer import BigramXGBoostScorer
+from wpm_conditioned_model import TypingModel
+
+qwerty = "qwertyuiopasdfghjkl'zxcvbnm,.-"
+tusk_1 = "tskcwjqioumvypznreaxlfgdbh-'.,"
+tusk_2 = "tskcwzqioumvypjnreaxlfgdbh-'.,"
+
+def generate():
+    layout = qwerty
+    keyboard = Keyboard(layout)
+    scorer = BigramXGBoostScorer()
+    optimizer = Optimizer(keyboard, scorer)
+    optimizer.local_improvement_3opt(keyboard, scorer)
+    print("Fitness", optimizer.best_fitness)
+    print(optimizer.best_keyboard)
+
+
+def main():
+    generate()
+
+if __name__ == "__main__":
+    main()
